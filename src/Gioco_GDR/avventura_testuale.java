f7b531db9da4733e31509563636b2c4ddf25736c
Administrator <Administrator@Administrator>
2016-11-20 05:03:22 +0100

    gioco di fabio

--- src/Gioco_GDR/avventura_testuale.java
+++ src/Gioco_GDR/avventura_testuale.java
@@ -20,26 +20,46 @@ public class avventura_testuale {
     public static void main(String[] args) {
         // TODO code application logic here
         JOptionPane.showMessageDialog(null, "Benvenuto in questa entusiasmate\n             avventura testuale");
-        String NomeCognome[] = new String[3];
+        //tutti i parametri per gestire il gioco
+        String NomeCognome[] = new String[3];//stringa nome e cognome
+
+        //parametri per la classe
+        int Classe;
+        String classi[] = new String[]{"Armigero", "Magicante", "Shadow", "Elementalista"};//4
+
         boolean controllo = false;
+
+        //parametri per statistiche
+        String statistiche[] = new String[]{"Forza", "Difesa", "Costituzione", "Intelligenza", "Agilità", "Mod. Exp.", "Punti Vita", "Totale Armatura", "Cariche Magiche", "Cariche Elementali"};//Cariche Tecnologiche
+        int statistiche_valore[] = new int[statistiche.length];
+
         int i = 0, j = 0;
+
+        //parametri per le razze
         int Razza = 0;
-        String RazzaPersonaggio[] = new String[] {"Umano", "Nano", "Elfo", "Dracolide", "Vergheuden" };
-        
+        String RazzaPersonaggio[] = new String[]{"Umano", "Nano", "Elfo", "Dracolide", "Vergheuden"};//Faithy
+
         while (i < 3) {
             NomeCognome[i] = inserisciNome_Cognome(i);
             i++;
         }//fine while inserimento nome e cognome
 
         System.out.println("Nome personaggio: " + NomeCognome[0] + NomeCognome[1] + "\nCognome personaggio: " + NomeCognome[2] + "\n");
-
         Razza = scegliRazza();
-        
+
+        statistiche_valore = valoriRazza(statistiche_valore, Razza);
         System.out.println("razza personaggio: " + RazzaPersonaggio[Razza - 1] + "\n");
-        
+
+        for (i = 0; i < 10; i++) {
+            System.out.println(statistiche[i] + " : " + statistiche_valore[i] + "\n");
+
+        }
+
+        //scegliClasse();
+        //definisciStatistiche(statistiche[]);
     }
 
-    
+    //static caricaPartita(){}
     static String inserisciNome_Cognome(int i) {
 
         String NomeCognome = "";
@@ -70,11 +90,11 @@ public class avventura_testuale {
         return NomeCognome;
 
     }//fine inserisciNome_Cognome
-        
+
     static int scegliRazza() {
         String razza = "";
         int Razza = 0;//definisco la razza
-        
+
         do {//ciclo per visualizzare le informazioni sulle razze
 
             if (Razza == 0) {
@@ -88,11 +108,11 @@ public class avventura_testuale {
                 }
             }//if razza
         } while (Razza == 0);//while per visualizzare info sulle razze
-        
-        return Razza;
-    }//fine metodo scegliRazza()
 
-    static Integer spiega_razza(int Razza) {
+        return Razza;
+    }//fine scegliRazza()
+
+    static Integer spiega_razza(int Razza) {//switch per aver espiegazioni sulle razze
         if (Razza == 6) {
             String choice1 = JOptionPane.showInputDialog("Digitare:\n .1 per avere informazioni su 'Umano' \n .2 per avere infomazioni su 'Elfo'\n"
                     + " .3 per avere informazioni su 'Nano'\n .4 per avere informazioni su 'Dracolo'\n .5 per avere infomazioni su 'Vergheuden'\n"
@@ -116,7 +136,7 @@ public class avventura_testuale {
                     Razza = 0;
                     break;
                 case 4:
-                    JOptionPane.showMessageDialog(null, "La razza Dracolo offre un bonus alla forza e alla CA di +1 \n e hanno il danno a mani nude aumentato di una unità");
+                    JOptionPane.showMessageDialog(null, "La razza Dracolo offre un bonus alla forza e alla CA di +2 \n e hanno il danno a mani nude aumentato di una unità");
                     JOptionPane.showMessageDialog(null, "Discendenti dei draghi, con ali e coda,\nvivono nell'attesa del risveglio del loro soffio elementale");
                     Razza = 0;
                     break;
@@ -135,7 +155,86 @@ public class avventura_testuale {
         return Razza;
 
     }
-    
+
+    static int[] valoriRazza(int[] statistiche_valore, int Razza) {
+
+        switch (Razza) {
+            case 1://umano
+                for (int i = 0; i < 10; i++) {
+                    statistiche_valore[i] = 0;
+                    if (i == 5) {
+                        statistiche_valore[i] += 10;
+                    }
+                }
+
+                return statistiche_valore;
+
+            case 2://elfo
+
+                for (int i = 0; i < 10; i++) {
+                    statistiche_valore[i] = 0;
+                    if (i == 4) {//bonus agilità
+                        statistiche_valore[i] += 1;
+                    }
+                    if (i == 2) {//malus costituzione
+                        statistiche_valore[i] -= 1;
+                    }
+
+                }
+
+                return statistiche_valore;
+
+            case 3://nano
+
+                for (int i = 0; i < 10; i++) {
+                    statistiche_valore[i] = 0;
+                    if (i == 4) {//malus agilità
+                        statistiche_valore[i] -= 1;
+                    }
+                    if (i == 2) {//bonus costituzione
+                        statistiche_valore[i] += 1;
+                    }
+
+                }
+
+                return statistiche_valore;
+
+            case 4://dracolide
+
+                for (int i = 0; i < 10; i++) {
+                    statistiche_valore[i] = 0;
+                    if (i == 0) {//bonus forza
+                        statistiche_valore[i] += 1;
+                    }
+                    if (i == 7) {//bonus totale armatura
+                        statistiche_valore[i] += 2;
+                    }
+
+                }
+
+                return statistiche_valore;
+
+            case 5://vergheuden
+
+                for (int i = 0; i < 10; i++) {
+                    statistiche_valore[i] = 0;
+                    if (i == 7) {//bonus totale armatura
+                        statistiche_valore[i] += 3;
+                    }
+                }
+
+                return statistiche_valore;
+
+        }
+
+    }
+
+    /*static int definisciStatistiche(int statistiche[]) {
+
+        String input;
+        input = JOptionPane.showInputDialog(
+    }
+
 }
 
 /*public scegli_classe_con_cui_giocare (){
